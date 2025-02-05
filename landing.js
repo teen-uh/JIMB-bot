0018247cb3cff7c4fdbcd64811a7aa7e8ec5d0a6
teen-uh <tinaytshi@gmail.com>
2025-02-04 17:58:49 -0800

    add loading

---
+++ landing.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(window).on("load", function () {
+    $("#icon").delay(1200).animate({ opacity: 1 }, 1200);
+    $("#icon-text").delay(1200).animate({ opacity: 1 }, 1200);
+  });
+});
