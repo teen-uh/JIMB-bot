bb7be235f3ed70cf2b177ea91d80b371f3e6beb4
teen-uh <tinaytshi@gmail.com>
2025-03-03 22:38:18 -0800

    big updates

--- landing.js
+++ landing.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     $("#loading-gif").delay(4000).fadeOut(500);
     $("#icon").delay(5000).animate({ opacity: 1 }, 1200);
     $("#icon-text").delay(5000).animate({ opacity: 1 }, 1200);
-    $("#start").delay(5500).animate({ opacity: 1 }, 1200);
+    $("#summary").delay(5500).animate({ opacity: 1 }, 1600);
+    $("#start").delay(6700).animate({ opacity: 1 }, 1200);
   });
 });
--- script.js
+++ script.js
@@ -13,6 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
   let dataDisplay = document.getElementById("chat");
   dataDisplay.innerHTML = "";
   let typingID = 1;
+  let option_a = 0,
+    option_b = 0,
+    option_c = 0,
+    option_d = 0;
+  let userName;
 
   function generateUserID() {
     let timestamp = Date.now();
@@ -21,6 +26,29 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   const userID = generateUserID();
+
+  function type(text, timeout = 1200) {
+    let typingWrapper = document.createElement("div");
+    typingWrapper.innerHTML = `
+        <div class="flex">
+         <div id="bot-pfp"></div>
+        <div class="msg bot" id="typing${typingID}">
+          <div class="typing">
+              <div class="dot" style="--delay: 200ms"></div>
+              <div class="dot" style="--delay: 400ms"></div>
+              <div class="dot" style="--delay: 600ms"></div>
+        </div></div>
+        </div>`;
+
+    dataDisplay.prepend(typingWrapper);
+
+    let typingReplace = document.getElementById("typing" + typingID);
+    typingID++;
+
+    setTimeout(function () {
+      typingReplace.innerHTML = text;
+    }, timeout);
+  }
 
   function fetchQuestionsAndStartChat() {
     let questionsRef = firebase.database().ref("questions");
@@ -92,20 +120,10 @@ document.addEventListener("DOMContentLoaded", function () {
             });
           });
         } else if (currentQuestion.type === "user-input") {
+          console.log("help");
           let userInput = document.getElementById("userResponse");
-          userInput.removeAttribute("disabled");
-          // userInput.addEventListener("submit", (e) => {
-          //   e.preventDefault();
-          //   let userAnswer = userInput.value;
-          //   if (userAnswer.trim() !== "") {
-          //     dataDisplay.innerHTML =
-          //       `<div class="msg user">${userAnswer}</div>` +
-          //       dataDisplay.innerHTML;
-          //     setTimeout(function () {
-          //       handleResponse(userAnswer, currentQuestionKey, currentQuestion);
-          //     }, 1500);
-          //   }
-          // });
+          userInput.classList.remove("disabled");
+          userInput.placeholder = "Enter response here";
           userInput.addEventListener("keydown", (e) => {
             if (e.key === "Enter") {
               e.preventDefault(); // Prevent form submission
@@ -114,7 +132,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 dataDisplay.innerHTML =
                   `<div class="msg user">${userAnswer}</div>` +
                   dataDisplay.innerHTML;
-                userInput.value = ""; // Clear input after submission
+                userInput.value = "";
+                userInput.classList.add("disabled");
+                userInput.placeholder = "";
                 setTimeout(function () {
                   handleResponse(
                     userAnswer,
@@ -125,13 +145,13 @@ document.addEventListener("DOMContentLoaded", function () {
               }
             }
           });
-          userInput.setAttributeAttribute("disabled");
         }
       }, 2500);
     }
 
     function handleResponse(userInput, questionKey, question) {
       let userResponsesRef = firebase.database().ref(`responses/${userID}`);
+      let currentQuestion = questions[questionKey];
       const responseData = {
         questionKey: questionKey,
         questionText: question.text,
@@ -158,6 +178,27 @@ document.addEventListener("DOMContentLoaded", function () {
       if (currentQuestion.type === "user-input") {
         responseKey = "default";
       } else if (currentQuestion.type === "multiple-choice") {
+        if (userInput === "a") {
+          option_a++;
+        } else if (userInput === "b") {
+          option_b++;
+        } else if (userInput === "c") {
+          option_c++;
+        } else if (userInput === "d") {
+          option_d++;
+        }
+
+        let responseLog =
+          "A: " +
+          option_a +
+          " B: " +
+          option_b +
+          " C: " +
+          option_c +
+          " D: " +
+          option_d;
+
+        console.log(responseLog);
         responseKey = userInput;
       }
 
@@ -169,8 +210,17 @@ document.addEventListener("DOMContentLoaded", function () {
       typingID++;
       currentQuestionIndex++;
       if (currentQuestionIndex < questionKeys.length) {
-        setTimeout(displayQuestion, 3200); // Wait before showing the next question
-      } else {
+        setTimeout(displayQuestion, 3200);
+      } else if (currentQuestionIndex >= questionKeys.length) {
+        let highestChoice = [
+          { val: option_a, variableName: "A" },
+          { val: option_b, variableName: "B" },
+          { val: option_c, variableName: "C" },
+          { val: option_d, variableName: "D" },
+        ];
+        let sortedChoices = highestChoice.sort((a, b) => a[val] - b[val]);
+        console.log(sortedChoices[3]["variableName"]);
+
         dataDisplay.prepend(
           '<div class="msg bot">Thank you for completing the chat! Your results are being calculated. Please proceed to the printer to receive your diagnosis.</div>'
         );
@@ -182,29 +232,19 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   setTimeout(function () {
-    let typingWrapper = document.createElement("div");
-    typingWrapper.innerHTML = `
-        <div class="flex">
-         <div id="bot-pfp"></div>
-        <div class="msg bot" id="typing${typingID}">
-          <div class="typing">
-              <div class="dot" style="--delay: 200ms"></div>
-              <div class="dot" style="--delay: 400ms"></div>
-              <div class="dot" style="--delay: 600ms"></div>
-        </div></div>
-        </div>`;
-
-    dataDisplay.prepend(typingWrapper);
-
-    let typingReplace = document.getElementById("typing" + typingID);
-
+    type("Hi! I'm JIMB-bot.");
     setTimeout(function () {
-      typingReplace.innerHTML =
-        "Hi! I'm JIMB-bot, a state of the art chatbot designed to help diagnose your time optimization and perception aptitude.";
-    }, 1300);
+      type(
+        "I am an advanced chatbot that evaluates your time optimization aptitude and present perception diagnosis.",
+        1200
+      );
+      setTimeout(function () {
+        type("Let's work together to optimize your productivity!", 1200);
+      }, 2900);
+    }, 2300);
   }, 500);
 
   setTimeout(function () {
     fetchQuestionsAndStartChat();
-  }, 1500);
+  }, 8400);
 });
