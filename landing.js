5073ef28f7f1766fd998db48de0cc2e29c9f13e6
teen-uh <tinaytshi@gmail.com>
2025-05-06 13:54:31 -0700

    updatee landing

--- landing.js
+++ landing.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-  $(window).on("load", function () {
-    $("#loading-gif").delay(4000).fadeOut(500);
-    $("#icon").delay(5000).animate({ opacity: 1 }, 1200);
-    $("#icon-text").delay(5000).animate({ opacity: 1 }, 1200);
-    $("#summary").delay(5500).animate({ opacity: 1 }, 1600);
-    $("#start").delay(6700).animate({ opacity: 1 }, 1200);
-  });
+  $("#loading-gif").delay(4000).fadeOut(500);
+  $("#loading").delay(4000).fadeOut(500);
+  $("#icon").delay(5000).animate({ opacity: 1 }, 1200);
+  $("#icon-text").delay(5000).animate({ opacity: 1 }, 1200);
+  $("#summary").delay(5500).animate({ opacity: 1 }, 1600);
+  $("#start").delay(6700).animate({ opacity: 1 }, 1200);
 });
